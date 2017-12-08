4cdbf3f83f3270435d9ad267f23edab5583f7951
Salomé <salome.maurin@gmail.com>
2017-12-08 19:40:24 +0100

    esqueleto codigo siguiente

--- Practica_3_DBA/src/practica_3_dba/Practica_3_DBA.java
+++ Practica_3_DBA/src/practica_3_dba/Practica_3_DBA.java
@@ -5,10 +5,16 @@
  */
 package practica_3_dba;
 
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.AgentsConnection;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+
 /**
  *
  * @author Sergio
  */
+
+
 public class Practica_3_DBA {
 
     /**
@@ -16,6 +22,17 @@ public class Practica_3_DBA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        SingleAgent vehiculo1, vehiculo2, vehiculo3, vehiculo4;
+        SingleAgent pizarra;
+        SingleAgent repostaje;
+        AgentsConnection.connect("isg2.ugr.es",6000,"Achernar","Leon","Matute",false);
+        System.out.println("\n");
+        //reconocimiento = new Reconocimiento(new AgentID("reconocimiento15"));
+        //repostaje = new Repostaje(new AgentID("repostaje15"));
+        //repostaje.start(); 
+        //reconocimiento.start();
+        //vehiculo = new Vehiculo(new AgentID("vehiculo15"));
+        //vehiculo.start();
     }
     
 }
