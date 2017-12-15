615f097d6397ec8e28f814d3810c5be7cf6f8485
alexrt96 <alexrt96@gmail.com>
2017-12-16 00:07:54 +0100

    Añadido actualizar mapa comun, actualizar datos y obtener datos. Añadido mi parte, solo falta pensar el movimiento y alguna cosilla mas

--- Practica_3_DBA/src/practica_3_dba/Vehiculo.java
+++ Practica_3_DBA/src/practica_3_dba/Vehiculo.java
@@ -11,6 +11,7 @@ import es.upv.dsic.gti_ia.core.AgentID;
 import es.upv.dsic.gti_ia.core.SingleAgent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 
 /**
@@ -20,7 +21,7 @@ import org.codehaus.jettison.json.JSONException;
 public class Vehiculo extends SingleAgent{
     
     private int fuelrate;
-    private boolean fly;
+    private int bateria;
     private int range;
     private int mapa[][];
     private int radar[][];
@@ -38,6 +39,8 @@ public class Vehiculo extends SingleAgent{
     private Tipo tipo;
     private String conversacion_id;
     private AgentID nombreVehiculo;
+    private String inReplyTo;
+    private int paso;
 
     /**
     *
@@ -48,64 +51,179 @@ public class Vehiculo extends SingleAgent{
         outbox = null;
         inbox = null;  
         finalizar = false;
+        inReplyTo = null;
+        conversacion_id = null;
         nombreVehiculo= aid;
+        mapa = new int[1000][1000];
+        paso = 0;
     }
     
     /**
     *
-    * @author 
+    * @author Alex
     */
-    public void conexion() throws InterruptedException, JSONException{
+    public boolean conexion() throws InterruptedException, JSONException{
+        //Recibimos el request de pizarra
         recibir_mensaje();
+        obtieneMapaComun();
+        //Construimos el mensaje
+        envio = new JSONObject();
+        envio.put("command","checkin");
+        //Enviamos el checkin al servidor
+        enviar_mensaje(envio.toString(),"Achernar",ACLMessage.REQUEST);
+        //Recibimos la respuesta del servidor
+        recibir_mensaje();
+        //Si da error, la conexión falla
+        if(inbox.getPerformativeInt()==ACLMessage.REFUSE || inbox.getPerformativeInt()==ACLMessage.NOT_UNDERSTOOD){
+            enviar_mensaje(recepcion.getString("details"), "pizarra", ACLMessage.REFUSE);
+            return false;
+        }//En caso contrario, la conexión tiene exito y enviamos a pizarra quien somos y que tipo somos
+        else{
+            JSONObject datos = recepcion.getJSONObject("Capabilities"); 
+            fuelrate = recepcion.getInt("fuelrate");
+            range = recepcion.getInt("range");
+            switch (range) {
+                case 7:
+                    tipo = Tipo.CAMION;
+                    break;
+                case 5:
+                    tipo = Tipo.COCHE;
+                    break;
+                default:
+                    tipo = Tipo.AEREO;
+                    break;
+            }
+            radar = new int [range][range];
+            envio = new JSONObject();
+            envio.put("TipoVehiculo",tipo);
+            envio.put("ID",nombreVehiculo);
+            enviar_mensaje("","Achernar",ACLMessage.QUERY_REF);
+            recibir_mensaje();
+            if(inbox.getPerformativeInt()==ACLMessage.NOT_UNDERSTOOD){
+                enviar_mensaje(" ", "pizarra", ACLMessage.REFUSE);
+                return false;
+            }else
+                actualizarDatos();
+            
+            envio = new JSONObject();
+            envio.put("TipoVehiculo",tipo);
+            envio.put("ID",nombreVehiculo);
+            //Avisamos a pizarra
+            enviar_mensaje(envio.toString(), "pizarra", ACLMessage.INFORM);
+            return true;
+        }
+            
     }
     
     /**
     *
-    * @author 
+    * @author Alex
     */
-    public void enviar_mensaje(String mensaje, String receptor){
+    public void actualizarDatos() throws JSONException{
+        JSONObject datos = recepcion.getJSONObject("result");
+        bateria = datos.getInt("battery");
+        gps_x = datos.getInt("x");
+        gps_y = datos.getInt("y");
+        JSONArray rad = datos.getJSONArray("sensor");
+        for(int i = 0; i < rad.length(); i+=range)
+            for(int j = 0; j < range; j++)
+                radar[i/range][j] = rad.getInt(i+j);
+        
+        System.out.print("Reconocimiento: actualizo radar ");
+        for(int i = 0; i < range; i++)
+            for(int j = 0; j < range; j++)
+                System.out.print(radar[i][j] + ", ");
+        
+        actualizarMapaComun();
+    }
+    
+    /**
+    *
+    * @author Alex
+    */
+    public void actualizarMapaComun(){
+        int medio;
+        if(range==7)
+            medio=3;
+        else if(range==5)
+            medio=2;
+        else
+            medio=1;
+        for(int i = 0; i < range; i++)
+            for(int j = 0; j < range; j++){
+                if(radar[i][j]==1)
+                    mapa[1000/2 + gps_y - medio + i][1000/2 + gps_x - medio + j] = 50000;
+            }
+        paso++;
+        mapa[1000/2 + gps_y][1000/2 + gps_x] = paso;
+    }
+    
+    /**
+    *
+    * @author Alex
+    */
+    public void obtieneMapaComun() throws JSONException{
+        JSONArray map = recepcion.getJSONArray("BuscaObjetivo");
+        paso = recepcion.getInt("Pasos");
+         for(int i = 0; i < map.length(); i+=1000)
+            for(int j = 0; j < 1000; j++)
+                radar[i/1000][j] = map.getInt(i+j);
+    }
+    
+    /**
+    *
+    * @author Alex Sergio Salomé Joaquín
+    */
+    public void enviar_mensaje(String mensaje, String receptor, int performativa){
+        System.out.println("Envia: " +mensaje+receptor);
         outbox = new ACLMessage();
         outbox.setSender(getAid());
+        //Para contestar con la id de la conversacion
+        if(conversacion_id!=null)
+            outbox.setConversationId(conversacion_id);
+        if(inReplyTo!=null && receptor.equals("Achernar"))
+            outbox.setInReplyTo(inReplyTo);
         outbox.setReceiver(new AgentID(receptor));
         outbox.setContent(mensaje);
+        outbox.setPerformative(performativa);
         this.send(outbox);
     }
     
     /**
     *
-    * @author 
+    * @author Alex Sergio Salomé Joaquín
+    * @throws java.lang.InterruptedException
     */
     public void recibir_mensaje() throws InterruptedException, JSONException{
         inbox = receiveACLMessage();
         recepcion = new JSONObject(inbox.getContent());
         String recepcion_plano = recepcion.toString();
-        System.out.println("Pizarra: " + recepcion_plano);
-        if(inbox.getPerformativeInt()==ACLMessage.REQUEST){
+        System.out.println("Vehiculo: " + recepcion_plano);
+        if(inbox.getPerformativeInt()==ACLMessage.REQUEST && conversacion_id == null){
             if(recepcion.has("ID")){
                 conversacion_id = recepcion.getString("ID");
             }
-        }
+        }else
+            inReplyTo = inbox.getReplyWith();
     }
     
     /**
     *
-    * @author 
+    * @author Alex
     */
     @Override
     public void execute(){
         try {
-            conexion();
-            /*while(!finalizar){
-            try {
-            recibir_mensaje();
-            actuar();
-            } catch (InterruptedException | JSONException ex) {
-            Logger.getLogger(Pizarra.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            }*/
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Vehiculo.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (JSONException ex) {
+            if(conexion()) 
+                while(!finalizar){
+                    try {
+                        recibir_mensaje();
+                        actuar();
+                    } catch (InterruptedException | JSONException ex) {
+                    Logger.getLogger(Pizarra.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+        } catch (InterruptedException | JSONException ex) {
             Logger.getLogger(Vehiculo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -125,21 +243,38 @@ public class Vehiculo extends SingleAgent{
 
     /**
     *
-    * @author a
+    * @author
+    * @throws org.codehaus.jettison.json.JSONException
+    * @throws java.lang.InterruptedException
     */
-    public void actuar() throws JSONException{
+    public void actuar() throws JSONException, InterruptedException{
         
-        if(recepcion.has("vehiculo")){
-            if(recepcion.getString("vehiculo").equals("cerrar"))
-                finalizar = true;
-        }else if(recepcion.has("fuelrate")){
-            envio = new JSONObject();
-            if(fuelrate <= 2)
-                envio.put("pizarra","Pizarra");
-            else//mandar mensaje ok a vehiculo
-                envio.put("pizarra","OK");
-            enviar_mensaje(envio.toString(), "vehiculo15");
+        //Busqueda
+        if(inbox.getPerformativeInt()==ACLMessage.REQUEST && inbox.getContent().equals("BuscaObjetivo")){
+            
+            if(fuelrate<=1){
+                //Codigo de salomé
+            }else{
+                //Pensar movimiento
+                
+                String movimiento = "moveN";
+                envio = new JSONObject();
+                envio.put("command",movimiento);
+                enviar_mensaje(envio.toString(),"Achernar",ACLMessage.REQUEST);
+                recibir_mensaje();
+                if(inbox.getPerformativeInt()==ACLMessage.FAILURE || inbox.getPerformativeInt()==ACLMessage.NOT_UNDERSTOOD){
+                    finalizar =true;
+                    enviar_mensaje(recepcion.getString("details"),"Achernar",ACLMessage.REFUSE);
+                }
+                else{
+                    //envio = new JSONObject();
+                    //envio.put("command",movimiento);
+                    enviar_mensaje(" ", "pizarra", ACLMessage.INFORM);
+                    
+                }
+            }
         }
+            
     }
 
 }
